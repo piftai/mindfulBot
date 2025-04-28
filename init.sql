7dbdd46fe89b7115db27601478713b06fdd466d2
piftai <piftaiwork@gmail.com>
2025-04-29 00:49:30 +0300

    corrected docker-compose file. now initing db with init.sql

---
+++ init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE admin (
+                       id SERIAL PRIMARY KEY,
+                       user_id BIGINT NOT NULL,
+                       username TEXT
+);
+
+CREATE TABLE reminders (
+                           id SERIAL PRIMARY KEY,
+                           user_id BIGINT NOT NULL,
+                           username TEXT,
+                           day TEXT NOT NULL,
+                           time TEXT NOT NULL,
+                           remind_1h TIMESTAMP NOT NULL,
+                           remind_24h TIMESTAMP NOT NULL
+);
+
+CREATE TABLE users (
+                       user_id BIGINT PRIMARY KEY,
+                       username TEXT,
+                       id SERIAL
+);
