532590625172328766b2e09e503ec45cef5f401a
piftai <piftaiwork@gmail.com>
2025-04-29 01:06:52 +0300

    corrected docker-compose file port 5432 closed? typo in init fixed

--- init.sql
+++ init.sql
@@ -11,7 +11,8 @@ CREATE TABLE reminders (
                            day TEXT NOT NULL,
                            time TEXT NOT NULL,
                            remind_1h TIMESTAMP NOT NULL,
-                           remind_24h TIMESTAMP NOT NULL
+                           remind_24h TIMESTAMP NOT NULL,
+                           is_always BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE users (
